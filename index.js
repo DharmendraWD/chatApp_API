82676c2c0e6d5eaf1837581b4a4f10541fc5e26b
Dharmendra <zaynhere@icloud.com>
2024-12-02 16:54:14 +0545

    changes port env

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/db");
 const userRouter = require("./routes/user.routes");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+require('dotenv').config();
 
 connectDB();
 
@@ -13,8 +14,10 @@ app.use("/api/users", userRouter);
 
 
 
+module.exports = (req, res) => {
+    app(req, res);
+};
 
 
 
-
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(process.env.PORT, () => console.log("Server running on port 3000"));
